2361111099bceeb2a9032317654fb669d4094451
Raymond Zhong <rzhong669@gmail.com>
2022-05-12 00:03:58 -0500

    Refactor grid code into function

--- javascript.js
+++ javascript.js
@@ -1,11 +1,15 @@
-const grid = document.querySelector('.grid');
-for(i=0; i<16; i++) { 
-  let newCol = document.createElement('div');
-  newCol.classList.add("column");
-  grid.appendChild(newCol);
-  for(j=0; j<16; j++) {
-    let box = document.createElement('div');
-    box.classList.add("box");
-    newCol.appendChild(box);
+function createGrid(col, row) {
+  const grid = document.querySelector('.grid');
+  for(i = 0; i < col; i++) { 
+    let newCol = document.createElement('div');
+    newCol.classList.add("column");
+    grid.appendChild(newCol);
+    for(j = 0; j < row; j++) {
+      let box = document.createElement('div');
+      box.classList.add("box");
+      newCol.appendChild(box);
+    }
   }
-}
+};
+
+createGrid(16, 16);
