bd1daf0d0e7ef720cb8194b2bdc69ea1bade80bd
Raymond Zhong <rzhong669@gmail.com>
2022-05-11 23:09:27 -0500

    Organize divs into a grid

--- javascript.js
+++ javascript.js
@@ -1,10 +1,11 @@
 const grid = document.querySelector('.grid');
 for(i=0; i<16; i++) { 
   let newRow = document.createElement('div');
+  newRow.classList.add("row");
   grid.appendChild(newRow);
-  for(j=0; j<2; j++) {
+  for(j=0; j<16; j++) {
     let div = document.createElement('div');
-    div.textContent = 'test';
+    div.textContent = '*';
     newRow.appendChild(div);
   }
 }
