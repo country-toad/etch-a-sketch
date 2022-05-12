e4430b9394a419f7123520f65bd631b5f033201e
Raymond Zhong <rzhong669@gmail.com>
2022-05-11 23:29:23 -0500

    Rename row to column

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
 const grid = document.querySelector('.grid');
 for(i=0; i<16; i++) { 
-  let newRow = document.createElement('div');
-  newRow.classList.add("row");
-  grid.appendChild(newRow);
+  let newCol = document.createElement('div');
+  newCol.classList.add("column");
+  grid.appendChild(newCol);
   for(j=0; j<16; j++) {
     let div = document.createElement('div');
     div.textContent = '*';
-    newRow.appendChild(div);
+    newCol.appendChild(div);
   }
 }
