ceef1a51a6f09f4160fb6d3f7dc238c4dc3708a1
Raymond Zhong <rzhong669@gmail.com>
2022-05-11 21:21:37 -0500

    Create nested divs

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
 const grid = document.querySelector('.grid');
-for(i = 0; i < 16*16; i++) { 
-  let div = document.createElement('div');
-  div.textContent = 'test';
-  grid.appendChild(div);
+for(i=0; i<16; i++) { 
+  let newRow = document.createElement('div');
+  grid.appendChild(newRow);
+  for(j=0; j<2; j++) {
+    let div = document.createElement('div');
+    div.textContent = 'test';
+    newRow.appendChild(div);
+  }
 }
