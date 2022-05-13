9e5cc912a0c1c9ba45277c4e6db73a5a687de13b
Raymond Zhong <rzhong669@gmail.com>
2022-05-13 00:20:18 -0500

    Create function that sets width and height for grid

--- javascript.js
+++ javascript.js
@@ -1,20 +1,31 @@
-function createGrid(col, row) {
+function createGrid(width=16, height=16) {
   const grid = document.querySelector('.grid');
-  for(i = 0; i < col; i++) { 
-    let newCol = document.createElement('div');
-    newCol.classList.add("column");
-    grid.appendChild(newCol);
-    for(j = 0; j < row; j++) {
+  for(i = 0; i < width; i++) { 
+    let column = document.createElement('div');
+    column.classList.add("column");
+    grid.appendChild(column);
+    for(j = 0; j < height; j++) {
       let box = document.createElement('div');
       box.classList.add("box");
-      box.addEventListener('mouseover', box => paint(box, 'lime'));
-      newCol.appendChild(box);
+      box.addEventListener('mouseover', box => paintBox(box, 'lime'));
+      column.appendChild(box);
     }
   }
 };
 
-function paint(item, color) {
+function paintBox(item, color) {
   item.target.style.backgroundColor = color;
 }
 
-createGrid(16, 16);
+const sizeButton = document.querySelector('.sizebutton');
+sizeButton.addEventListener('click', getSize);
+
+function getSize() {
+  let width = prompt("Set width:");
+  let height = prompt("Set height:");
+  if (width && height) { 
+    createGrid(width, height);
+  }
+}
+
+createGrid();
