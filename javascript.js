cdbbf4e68c3d916048fcf2d6181ad8da227def86
Raymond Zhong <rzhong669@gmail.com>
2022-05-14 11:21:25 -0500

    Remove previous grid when creating new one

--- javascript.js
+++ javascript.js
@@ -1,14 +1,17 @@
 function createGrid(width=16, height=16) {
+  if(document.querySelector('.grid')) {
+    removeGrid();
+  }
   const grid = document.createElement('div');
-  grid.classList.add("grid");
+  grid.classList.add('grid');
   document.body.appendChild(grid);
   for(i = 0; i < width; i++) { 
     let column = document.createElement('div');
-    column.classList.add("column");
+    column.classList.add('column');
     grid.appendChild(column);
     for(j = 0; j < height; j++) {
       let box = document.createElement('div');
-      box.classList.add("box");
+      box.classList.add('box');
       box.addEventListener('mouseover', box => paintBox(box, 'lime'));
       column.appendChild(box);
     }
@@ -23,11 +26,16 @@ const sizeButton = document.querySelector('.sizebutton');
 sizeButton.addEventListener('click', getSize);
 
 function getSize() {
-  let width = prompt("Set width:");
-  let height = prompt("Set height:");
+  let width = prompt('Set width:');
+  let height = prompt('Set height:');
   if (width && height) { 
     createGrid(width, height);
   }
+}
+
+function removeGrid() {
+  const grid = document.querySelector('.grid');
+  grid.remove();
 }
 
 createGrid();
